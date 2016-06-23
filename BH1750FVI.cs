96e8ec27b4c7dbf4e53d4a2529ff6ecdf9e0d83f
jeremylindsayni <jeremy.lindsay@outlook.com>
2016-06-23 19:47:14 +0100

    Initial commit.

---
+++ BH1750FVI.cs
@@ -0,0 +1,53 @@
+﻿using Magellanic.I2C;
+using System;
+using System.Threading.Tasks;
+
+namespace Magellanic.Sensors.BH1750FVI
+{
+    public class BH1750FVI : AbstractI2CDevice
+    {
+        private byte I2C_ADDRESS = 0x23;
+
+        public override byte[] GetDeviceId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte GetI2cAddress()
+        {
+            return I2C_ADDRESS;
+        }
+
+        public void PowerOn()
+        {
+            this.Slave.Write(new byte[] { 0x01 });
+        }
+
+        public void PowerOff()
+        {
+            this.Slave.Write(new byte[] { 0x00 });
+        }
+
+        public void Reset()
+        {
+            this.Slave.Write(new byte[] { 0x07 });
+        }
+
+        public void SetMode(MeasurementMode measurementMode)
+        {
+            this.Slave.Write(new byte[] { (byte)measurementMode });
+            Task.Delay(10).Wait();
+        }
+
+        public int GetLightLevel()
+        {
+            var readBuffer = new byte[2];
+
+            this.Slave.WriteRead(new byte[] { I2C_ADDRESS }, readBuffer);
+
+            var lightLevel = readBuffer[0] << 8 | readBuffer[1];
+
+            return (int)(((float)lightLevel) / 1.2f);
+        }
+    }
+}
---
+++ MeasurementMode.cs
@@ -0,0 +1,12 @@
+﻿namespace Magellanic.Sensors.BH1750FVI
+{
+    public enum MeasurementMode
+    {
+        ContinuouslyHighResolutionMode = 0x10,
+        ContinuouslyHighResolutionMode2 = 0x11,
+        ContinuouslyLowResolutionMode = 0x13,
+        OneTimeHighResolutionMode = 0x20,
+        OneTimeHighResolutionMode2 = 0x21,
+        OneTimeLowResolutionMode = 0x23
+    }
+}
